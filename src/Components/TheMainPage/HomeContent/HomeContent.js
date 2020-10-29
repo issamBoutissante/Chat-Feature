45c0dfd4b1431bfbfc730adfe90a34638c72ad55
Boutissante Issam <boutissante.issam.dev@gmail.com>
2020-10-29 12:53:11 +0100

    working on home content

---
+++ src/Components/TheMainPage/HomeContent/HomeContent.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from "./HomeContent.module.css";
+import Text from "../../messagingArea/Layout/MyTextInput/MyTextInput";
+
+export default function HomeContent(props) {
+  return (
+    <secion className={classes.HomeContent}>
+      <header className={classes.header}></header>
+      <section className={classes.main}>
+        <div className={classes.friendsArea}>
+          <div className={classes.text}>Friends</div>
+          <input type="text" className={classes.text} />
+        </div>
+        <div className={classes.messagingArea}></div>
+      </section>
+    </secion>
+  );
+}
