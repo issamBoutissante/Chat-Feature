05bc31e3ffbed1092db36e6a6ea284ea88bcaa77
issamBoutissante <boutissante.issam.dev@gmail.com>
2020-11-05 11:31:03 +0100

    still working on home page

--- src/Components/TheMainPage/HomeContent/HomeContent.js
+++ src/Components/TheMainPage/HomeContent/HomeContent.js
@@ -1,21 +1,36 @@
 import React from "react";
 import classes from "./HomeContent.module.css";
-import Text from "../../messagingArea/Layout/MyTextInput/MyTextInput";
+import Functions from "../../LogInPage/LogInFunctions/LogInFunctions";
 
-export default function HomeContent(props) {
-  return (
-    <secion className={classes.HomeContent}>
-      <header className={classes.header}></header>
-      <section className={classes.main}>
-        <div className={classes.friendsArea}>
-          <div className={classes.text}>Friends</div>
-          <input type="text" className={classes.text} />
-          <ul>
-            {/*<!--here i will add all the friends that the person will have */}
-          </ul>
-        </div>
-        <div className={classes.messagingArea}></div>
-      </section>
-    </secion>
-  );
+
+
+export default class HomeContent extends React.Component {
+  state={
+    friends={}
+  }
+  onSearchHandler=(name) => {
+  Functions.isAccountExist(name).then((value) => {
+    if (value.isExist) {
+      
+    }
+  });
+};
+  render() {
+    return (
+      <secion className={classes.HomeContent}>
+        <header className={classes.header}>
+          <h1>{props.accountInfo.name}</h1>
+        </header>
+        <section className={classes.main}>
+          <div className={classes.friendsArea}>
+            <div className={classes.text}>Friends</div>
+            <input type="text" className={classes.text} />
+            <button>Search</button>
+            <ul></ul>
+          </div>
+          <div className={classes.messagingArea}></div>
+        </section>
+      </secion>
+    );
+  }
 }
